c0ca2e571e86e2b46ded09046d9076bd7427396d
RMPR <akoudanilo@gmail.com>
2018-11-15 20:06:47 +0100

    Read from ods file module

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,7 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+import * as xlsx from "xlsx"
+
+let community = xlsx.readFile("data/4GI.ods").Sheets
+console.log(community)
