0b71a2a62b798198e9e898878fa81f01591446a5
RMPR <akoudanilo@gmail.com>
2018-11-23 09:42:59 +0100

    contribution history

--- scripts/viewMember.js
+++ scripts/viewMember.js
@@ -7,21 +7,46 @@ let contribution = document.getElementById("contribution")
 let personName = localStorage.getItem("view")
 
 let index = 0
-for (let i=1; i<=parseInt(localStorage.getItem("size")); i++){
-    if (localStorage.getItem('A'+i) === (personName)){
+for (let i = 1; i <= parseInt(localStorage.getItem("size")); i++) {
+    if (localStorage.getItem('A' + i) === (personName)) {
         index = i
     }
 }
 
-name.innerHTML = localStorage.getItem("A"+index)
-numero.innerHTML = localStorage.getItem("B"+index)
-email.innerHTML = localStorage.getItem("C"+index)
-entreprise.innerHTML = localStorage.getItem("D"+index)
+name.innerHTML = localStorage.getItem("A" + index)
+numero.innerHTML = localStorage.getItem("B" + index)
+email.innerHTML = localStorage.getItem("C" + index)
+entreprise.innerHTML = localStorage.getItem("D" + index)
 let somme = 0
-for(const key of localStorage.getItem('E'+index).split(" ")){
-    if (key === ""){
+for (const key of localStorage.getItem('E' + index).split(" ")) {
+    if (key === "") {
         continue
     }
-    somme += parseInt(key) 
+    somme += parseInt(key)
 }
-contribution.innerHTML = (somme)+""
+contribution.innerHTML = (somme) + ""
+
+let myTable = document.getElementById("myTable")
+
+let row = document.createElement("tr")
+let cell1 = document.createElement("td")
+let cell2 = document.createElement("td")
+let money = localStorage.getItem("E" + index).split(" ")
+let i = 0
+for (const dateContribution of localStorage.getItem("F" + index).split(" ")) {
+    if (money[i] !== "") {
+        textnode1 = document.createTextNode(money[i]);
+    } else {
+        textnode1 = document.createTextNode(money[++i]);
+    }
+    cell1.appendChild(textnode1)
+    textnode2 = document.createTextNode(dateContribution);
+    cell2.appendChild(textnode2)
+    row.appendChild(cell1)
+    row.appendChild(cell2)
+    myTable.appendChild(row)
+    cell1 = document.createElement("td")
+    cell2 = document.createElement("td")
+    row = document.createElement("tr")
+    ++i
+}
